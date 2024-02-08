f991a7ed2353c562180a88e45ca9aebadd648675
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-02-08 23:33:38 +0530

    tower of hanoi updated

--- Practicals/Tower_of_hanoi.c
+++ Practicals/Tower_of_hanoi.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
-
-// Function to move disks between towers
-void moveDisk(char source, char dest, int disk) {
-    printf("Move disk %d from %c to %c\n", disk, source, dest);
-}
-
-// Tower of Hanoi function using recursion
-void towerOfHanoi(int num_disks, char source, char aux, char dest) {
-    if (num_disks == 1) {
-        moveDisk(source, dest, 1);
-        return;
-    }
-
-    towerOfHanoi(num_disks - 1, source, dest, aux);
-    moveDisk(source, dest, num_disks);
-    towerOfHanoi(num_disks - 1, aux, source, dest);
-}
-
-int main() {
-    int num_disks = 3; // Number of disks
-    char source = 'A', aux = 'B', dest = 'C';
-
-    // Solve Tower of Hanoi problem
-    towerOfHanoi(num_disks, source, aux, dest);
-
+void towers(int,char,char,char);
+int main(){
+    int num;
+    printf("Enter the nmber of disks:");
+    scanf("%d",&num);
+    printf("The sequence moves involved in the Tower of hanoi are:\n");
+    towers(num,'A','C','B');
     return 0;
 }
+void towers(int num,char source,char destination,char aux){
+    if(num==1){
+        printf("\nMoves disks 1 from %c to ring %c",source,destination);
+        return ;
+    }
+    else{
+        towers(num-1,source,aux,destination);
+        printf("\nMoves disks %d from %c to ring %c",num,source,destination);
+        towers(num-1,aux,destination,source);
+        
 
 
+    }
+
+}
